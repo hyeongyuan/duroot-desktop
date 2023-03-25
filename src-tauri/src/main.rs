52a5a3eaf0cf360010f4a15fdeb274283f25a85f
thomas.an <hyeongyu2656@gmail.com>
2023-03-25 19:20:59 +0900

    feat: 트레이 메뉴 및 윈도우 생성 추가

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,46 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use tauri::{SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, CustomMenuItem};
+
+fn generate_tray_menu() -> SystemTrayMenu {
+    let version = CustomMenuItem::new("version".to_string(), "v0.0.1").disabled();
+    let preference = CustomMenuItem::new("preference".to_string(), "설정");
+    let quit = CustomMenuItem::new("quit".to_string() , "종료");
+
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(version)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(preference)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    
+    tray_menu
 }
 
 fn main() {
+    let tray_menu = generate_tray_menu();
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                match id.as_str() {
+                    "preference" => {
+                        tauri::WindowBuilder::new(
+                            app,
+                            "Main",
+                            tauri::WindowUrl::App("index.html".into())
+                        ).build().expect("failed to build window");
+                    }
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
