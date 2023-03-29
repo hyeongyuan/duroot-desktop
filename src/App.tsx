62aa9ecca60a153b3ee2d23dd18b17d64e0ce6d8
thomas.an <hyeongyu2656@gmail.com>
2023-03-30 00:26:32 +0900

    feat: 유지 가능한 데이터베이스 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/solid-query';
-import { createSignal, Show } from 'solid-js';
+import { createEffect, createSignal, Show } from 'solid-js';
 import AuthPage from './pages/auth';
 import MainPage from './pages/main';
+import { getAllDatabase, IDatabase, updateDatabase } from './utils/database';
 
 const queryClient = new QueryClient();
 
 function App() {
-  const [token, setToken] = createSignal();
+  const [database, setDatabase] = createSignal<IDatabase>();
+
+  createEffect(async () => {
+    const data = await getAllDatabase();
+    
+    setDatabase(data || {});
+  });
+
+  const handleSubmit = async (token: string) => {
+    await updateDatabase('token.github', token);
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
       <div class="flex bg-[#2f2f2f] text-[#f6f6f6] h-screen rounded">
-        <Show when={!!token()} fallback={<AuthPage onSubmit={setToken} />}>
-          <MainPage />
+        <Show when={!!database()} fallback={<div>Loading ...</div>}>
+          {!database()?.token?.github ? (
+            <AuthPage onSubmit={handleSubmit} />
+          ) : (
+            <MainPage />
+          )}
         </Show>
       </div>
     </QueryClientProvider>
---
+++ src/utils/database.ts
@@ -0,0 +1,37 @@
+import { exists, writeTextFile, readTextFile, BaseDirectory } from '@tauri-apps/api/fs';
+
+const DATABASE_ROOT = 'databases';
+const DATABASE_FILE = `${DATABASE_ROOT}/v1.txt`;
+
+export interface IDatabase {
+  token?: {
+    github?: string;
+  };
+}
+
+export const getAllDatabase = async (): Promise<IDatabase | null> => {
+  const exitsFile = await exists(DATABASE_FILE, { dir: BaseDirectory.AppData });
+  if (!exitsFile) {
+    return null;
+  }
+  const text = await readTextFile(DATABASE_FILE, { dir: BaseDirectory.AppData });
+  return JSON.parse(text);
+};
+
+export const updateDatabase = async (key: string, value: any): Promise<IDatabase> => {
+  const data = (await getAllDatabase() || {});
+
+  let pointer = data as any;
+  const keys = key.split('.');
+
+  for (let i = 0; i < keys.length; i++) {
+    const currentKey = keys[i];
+    if (i === keys.length - 1) {
+      pointer[currentKey] = value;
+    } else {
+      pointer = pointer[currentKey] || {};
+    }
+  }
+  await writeTextFile(DATABASE_FILE, JSON.stringify(data), { dir: BaseDirectory.AppData });
+  return data;
+};
