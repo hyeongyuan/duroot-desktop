600095cc64fcd7850618c6b755920e695f4d378e
thomas.an <hyeongyu2656@gmail.com>
2023-04-04 00:53:31 +0900

    feat: 데이터 조회 일자 표시

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,5 @@
-import { createEffect, createSignal, For } from 'solid-js';
-import { formatDistanceToNow } from 'date-fns';
+import { batch, createEffect, createSignal, For, Show } from 'solid-js';
+import { format, formatDistanceToNow } from 'date-fns';
 import { PullItem } from '../components/github/pull-item';
 import { Spinner } from '../components/spinner';
 import { createLocalStorageSignal } from '../hooks/createLocal-storage-signal';
@@ -9,6 +9,7 @@ import { PullRequestListViewItem } from '../models/pull-request-list-view-item';
 function Main() {
   const [token] = createLocalStorageSignal<{github: string}>('token');
   const [pullRequests, setPullRequests] = createSignal<PullRequestListViewItem[]>();
+  const [lastUpdatedAt, setLastUpdatedAt] = createSignal<Date>();
   
   createEffect(() => {
     const githubToken = token()?.github;
@@ -20,7 +21,10 @@ function Main() {
             return viewItem.loadReviewerCount(githubToken).then(() => viewItem);
           }));
 
-          setPullRequests(viewItems);
+          batch(() => {
+            setPullRequests(viewItems);
+            setLastUpdatedAt(new Date());
+          });
         });
     }
   });
@@ -31,16 +35,23 @@ function Main() {
   
   return (
     <div class="w-full">
-      <div
-        class="bg-[#2d333b] px-1 pt-1 border border-[#373e47]"
-        onClick={() => handleClickAdd()}
-      >
-        <div class="inline-block p-1 cursor-pointer">
+      <div class="bg-[#2d333b] px-1 pt-1 border border-[#373e47]">
+        <div
+          class="inline-block p-1 cursor-pointer"
+          onClick={() => handleClickAdd()}
+        >
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6 12H18M12 18V6" stroke="#768390" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         </div>
       </div>
+      <Show when={!!lastUpdatedAt()}>
+        <div class="py-2">
+          <p class="text-[#768390] text-[10px] text-center">
+            {`마지막 업데이트 ${format(lastUpdatedAt() as Date, 'yyyy년 MM월 dd일 HH시 mm분')}`}
+          </p>
+        </div>
+      </Show>
       <ul class="divide-y divide-[#373e47]">
         <For each={pullRequests()} fallback={<Spinner />}>
           {item => (
