1296f22ad9bb12fbe59bf17eb12201cecf71d0a3
thomas.an <hyeongyu2656@gmail.com>
2023-04-30 17:57:07 +0900

    update: gist 정보를 가져오는 API 추가

--- .github/actions/update-gist/index.js
+++ .github/actions/update-gist/index.js
@@ -1,12 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
-try {
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const GIST_ID = '8bd4a8d0481a7be2d657d8846f78b20c';
+
+const run = async () => {
+  try {
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  
+    const { data } = await axios.get(`https://api.github.com/gists/${GIST_ID}`);
+
+    console.log(JSON.stringify(data, undefined, 2));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+
+run();
