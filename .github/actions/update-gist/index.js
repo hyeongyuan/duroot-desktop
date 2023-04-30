56ed01c36a0d04880a67d9580c12ffdecef0abfd
thomas.an <hyeongyu2656@gmail.com>
2023-04-30 18:29:19 +0900

    update: signature 정보 가져오도록 수정

--- .github/actions/update-gist/index.js
+++ .github/actions/update-gist/index.js
@@ -6,21 +6,36 @@ const GIST_ID = '8bd4a8d0481a7be2d657d8846f78b20c';
 const FILE_NAME = 'lastest-version.json';
 const PLATFORM_MAC = 'darwin-x86_64';
 
+const getGist = async id => {
+  const { data } = await axios.get(`https://api.github.com/gists/${id}`);
+  const { content } = data.files[FILE_NAME];
+
+  return JSON.parse(content);
+};
+
 const run = async () => {
   try {
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
+
+    const { release: { tag_name, assets } } = github.context.payload;
+    const [, nextVersion] = tag_name.split('-');
+
+    const sigAsset = assets.find(asset => asset.name.includes('.sig'));
+
+    const { data: sigData } = await axios.get(sigAsset.browser_download_url);
+
+    console.log(sigData);
   
-    const { data } = await axios.get(`https://api.github.com/gists/${GIST_ID}`);
-    const { content } = data.files[FILE_NAME];
+    const latestVersion = await getGist(GIST_ID);
 
-    const { version, platforms } = JSON.parse(content);
-    const { signature, url } = platforms[PLATFORM_MAC];
+    latestVersion.version = nextVersion;
+    latestVersion.pub_date = new Date().toISOString();
+    latestVersion.platforms[PLATFORM_MAC].url = `https://github.com/hyeongyuan/duroot-desktop/releases/download/release-${nextVersion}/Duroot.app.tar.gz`;
+    latestVersion.platforms[PLATFORM_MAC].signature = 'new sig';
 
-    console.log(JSON.stringify({version, signature, url}, undefined, 2));
+    console.log(JSON.stringify(latestVersion, undefined, 2));
   } catch (error) {
     core.setFailed(error.message);
   }
