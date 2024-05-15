4daa0a15c473def54521c1b22020d4e114b4109f
hyeongyu An <hyeongyu2656@gmail.com>
2024-05-15 17:58:30 +0900

    feat: Input 공통 컴포넌트 추가

---
+++ src/components/common/input.tsx
@@ -0,0 +1,17 @@
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input({ value, placeholder, onChange }: InputProps) {
+  return (
+    <div className="bg-[#1c2128] text-[#adbac7] text-sm border border-[#444c56] rounded h-[33px] px-2">
+      <input
+        type="text"
+        className="bg-transparent w-full h-full"
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
