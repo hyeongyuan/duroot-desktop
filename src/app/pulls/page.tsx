f0a99455b6558de0ede9462484e68fe8b0ee5335
hyeongyu An <hyeongyu2656@gmail.com>
2024-05-25 16:10:14 +0900

    feat: withAuth hoc 추가 및 적용

--- src/app/pulls/page.tsx
+++ src/app/pulls/page.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import { useAuthStore } from '@/stores/auth';
 import { PullsTabs } from '@/components/github/pulls-tabs';
 import { Header } from '@/components/github/header';
+import { withAuth } from '@/hoc/with-auth';
 
-export default function Pulls() {
-  const { data } = useAuthStore();
-
-  if (!data) {
-    return null;
-  }
+function Pulls() {
   return (
     <div className="w-full">
       <Header />
       <PullsTabs />
-      <div>{data.id}</div>
     </div>
   );
 }
+
+export default withAuth(Pulls);
---
+++ src/hoc/with-auth.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useAuthStore } from '@/stores/auth';
+import { database } from '@/utils/database';
+import { fetchUser } from '@/apis/github';
+import { type GithubUser } from '@/types/github';
+import { useRouter } from 'next/navigation';
+
+export interface AuthProps {
+  auth: GithubUser;
+}
+
+export const withAuth = <P extends AuthProps>(WrappedComponent: React.ComponentType<P>) => {
+  const Component = (props: Omit<P, keyof AuthProps>) => {
+    const router = useRouter();
+    const { data, setData } = useAuthStore();
+
+    useEffect(() => {
+      if (data) {
+        return;
+      }
+      database.getFieldValue<string>('token.github').then(async (token) => {
+        if (!token) {
+          router.replace('/auth');
+          return;
+        }
+        try {
+          const user = await fetchUser(token);
+          setData(user);
+        } catch (error) {
+          router.replace('/auth');
+        }
+      });
+    }, [router, data, setData]);
+
+    if (!data) {
+      return null;
+    }
+    return <WrappedComponent {...(props as P)} auth={data} />;
+  };
+  return Component;
+};
