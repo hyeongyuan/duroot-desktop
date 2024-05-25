f53a94b0d2afec370f83cdedcf95fdfee1471b48
hyeongyu An <hyeongyu2656@gmail.com>
2024-05-25 14:21:57 +0900

    feat: fs 이용한 데이터베이스 기능 추가

---
+++ src/utils/database.ts
@@ -0,0 +1,47 @@
+import { exists, readTextFile, BaseDirectory } from '@tauri-apps/api/fs';
+
+const DATABASE_ROOT = 'databases';
+const DATABASE_FILE = `${DATABASE_ROOT}/v1.txt`;
+
+export interface DatabaseData {
+  token?: {
+    github?: string;
+  };
+}
+
+class Database {
+  private _data?: DatabaseData;
+
+  private async _load() {
+    const exitsFile = await exists(DATABASE_FILE, { dir: BaseDirectory.AppData });
+    if (!exitsFile) {
+      return {};
+    }
+    const text = await readTextFile(DATABASE_FILE, { dir: BaseDirectory.AppData });
+    return JSON.parse(text) as DatabaseData;
+  }
+
+  private async _getData() {
+    if (this._data !== undefined) {
+      return this._data;
+    }
+    const data = await this._load();
+    this._data = data;
+    return data;
+  }
+
+  async getFieldValue<T>(key: string) {
+    const data = await this._getData();
+
+    let pointer = data as any;
+    const keys = key.split('.');
+
+    for (let i = 0; i < keys.length; i++) {
+      const currentKey = keys[i];
+      pointer = pointer[currentKey] || {};
+    }
+    return pointer as T | undefined;
+  }
+}
+
+export const database = new Database();
