6dc5271101bea352bfd82e68162d523c29f6cc4d
hyeongyu An <hyeongyu2656@gmail.com>
2024-05-25 15:28:39 +0900

    feat: 탭 컴포넌트 추가

---
+++ src/components/common/tabs.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+
+const TAB_LIST_HEIGHT = 44;
+
+export interface Tab {
+  key: string;
+  name: string;
+  href: string;
+  count?: number;
+}
+
+interface TabsProps {
+  data: Tab[];
+  activeTab: Tab['key'];
+  shallowRouting?: boolean;
+}
+
+export function Tabs({ data, activeTab, shallowRouting }: TabsProps) {
+  return (
+    <ul
+      style={{
+        height: `${TAB_LIST_HEIGHT}px`,
+        boxShadow: 'inset 0 -1px 0 #373e47'
+      }}
+      className="px-2 overflow-y-auto whitespace-nowrap"
+    >
+      {data.map(({ key, name, href, count }) => (
+        <li key ={key} className={`py-2 ${activeTab === key ? 'border-[#ec775c]' : 'border-[transparent]'} border-b-2 inline-block`}>
+          <Link
+            href={href}
+            className={`p-2 text-xs cursor-pointer ${activeTab === key ? 'text-[#e6edf3]': ''} hover:text-[#e6edf3]`}
+            shallow={shallowRouting}
+          >
+            {name}
+            {!!count && (
+              <span className="ml-2 px-1 bg-[rgba(99,110,123,0.4)] rounded-full">
+                {count}
+              </span>
+            )}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/components/github/pulls-tabs.tsx
@@ -0,0 +1,41 @@
+import { Tab, Tabs } from '@/components/common/tabs';
+import { useSearchParams } from 'next/navigation';
+
+enum TabKey {
+  MY_PULL_REQUESTS = 'myPullRequests',
+  REQUESTED_PULL_REQUESTS = 'requestedPullRequests',
+  REVIEWED_PULL_REQUESTS = 'reviewedPullRequests',
+  APPROVED_PULL_REQUESTS = 'approvedPullRequests',
+}
+
+const TABS: Tab[] =  [
+  {
+    key: TabKey.MY_PULL_REQUESTS,
+    name: 'My',
+    href: `/pulls?tab=${TabKey.MY_PULL_REQUESTS}`,
+  },
+  {
+    key: TabKey.REQUESTED_PULL_REQUESTS,
+    name: 'Requested',
+    href: `/pulls?tab=${TabKey.REQUESTED_PULL_REQUESTS}`,
+  },
+  {
+    key: TabKey.REVIEWED_PULL_REQUESTS,
+    name: 'Reviewed',
+    href: `/pulls?tab=${TabKey.REVIEWED_PULL_REQUESTS}`,
+  },
+  {
+    key: TabKey.APPROVED_PULL_REQUESTS,
+    name: 'Approved',
+    href: `/pulls?tab=${TabKey.APPROVED_PULL_REQUESTS}`,
+  }
+];
+
+export function PullsTabs() {
+  const searchParams = useSearchParams();
+  const tabQuery = (searchParams.get('tab') || TabKey.MY_PULL_REQUESTS)as TabKey;
+
+  return (
+    <Tabs data={TABS} activeTab={tabQuery} />
+  );
+}
