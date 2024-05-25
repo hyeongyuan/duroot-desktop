ed064d9636d1f537f2a095aa1e58642d40d31d16
hyeongyu An <hyeongyu2656@gmail.com>
2024-05-25 14:22:34 +0900

    feat: auth 스토어 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,28 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/stores/auth';
+import { database } from '@/utils/database';
+import { fetchUser } from '@/apis/github';
 
 export default function Home() {
   const router = useRouter();
+  const auth = useAuthStore();
 
   useEffect(() => {
-    router.replace('/auth');
-  }, [router]);
+    database.getFieldValue<string>('token.github').then(async (token) => {
+      if (!token) {
+        router.replace('/auth');
+        return;
+      }
+      try {
+        const user = await fetchUser(token);
+        console.log(user);
+      } catch (error) {
+        router.replace('/auth');
+      }
+    });
+  }, [auth, router]);
 
   return null;
 }
---
+++ src/stores/auth.ts
@@ -0,0 +1,12 @@
+import { GithubUser } from '@/types/github';
+import { create } from 'zustand';
+
+interface AuthStare {
+  data: GithubUser | null
+  setData: (data: GithubUser) => void;
+}
+
+export const useAuthStore = create<AuthStare>((set) => ({
+  data: null,
+  setData: (data) => set({ data })
+}));
