ba28d58b93789a91ff3cf9db114ea7b1567712b1
hyeongyu An <hyeongyu2656@gmail.com>
2024-07-16 02:09:40 +0900

    fix: 테스트를 위해 추가했던 로직 제거

--- src/components/github/pulls-list.tsx
+++ src/components/github/pulls-list.tsx
@@ -74,7 +74,7 @@ export function PullsList() {
             <RoundButton onClick={handleClickOpenAll} label="Open all"  />
           </div>
           <ul className="divide-y divide-[#373e47]">
-            {[...pulls.items, ...pulls.items, ...pulls.items, ...pulls.items].map((pull => {
+            {pulls.items.map((pull => {
               const [repo, owner] = pull.repository_url.split('/').reverse();
               const ownerRepo = `${owner}/${repo}`;
               const labels = pull.draft
